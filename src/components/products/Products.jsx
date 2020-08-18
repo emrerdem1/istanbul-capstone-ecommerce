a29694e97a6d274d45cbc69c045d4ed4ceb793d2
emrerdem1 <emrerdem94@gmail.com>
2020-08-19 00:22:24 +0300

    Fix: fix extra slash symbol

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -4,16 +4,23 @@ import { Row } from "react-bootstrap";
 import { MOCK_DATABASE } from "../common/MockDatabase";
 import "./style.scss";
 import { useLocation } from "react-router-dom";
+import { HOME, PRODUCTS } from "../../containers/Route.paths";
+
+const TEXT = "products";
 
 export default function Products() {
   // We don't want to use a dynamic path finder like useRouteMatch because we are having this component in HomePage as well.
   // In that case, it would render the `path` and `url` as `/` which will not work in our favour.
 
   const { pathname } = useLocation();
+  let cutPathAfterSlash = `${pathname.slice(12)} / ${TEXT}`;
+
+  const sectionTitle =
+    pathname === HOME || pathname === PRODUCTS ? TEXT : cutPathAfterSlash;
 
   return (
     <div className="productsSectionContainer">
-      <h1 className="productsTitle">{pathname.slice(12)}/products</h1>
+      <h1 className="productsTitle">{sectionTitle}</h1>
       <Row>
         {MOCK_DATABASE.map((info, index) => (
           <ProductCard info={info} index={index} />
