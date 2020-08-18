3671e81a15532ae3348509ed00bc47342d9bea7e
mira_arar <mira.ar230@gmail.com>
2020-08-18 15:59:47 +0300

    correcting a typo

--- src/components/auth/signUp/SignUp.jsx
+++ src/components/auth/signUp/SignUp.jsx
@@ -48,7 +48,7 @@ export default function SignUp() {
               name="email"
               type="email"
               className="signUpFormInput"
-              requirednk
+              required
             />
           </label>
 
@@ -62,7 +62,6 @@ export default function SignUp() {
             />
           </label>
           <NavLink to="/">
-            {" "}
             <button className="signUpFormBtn">Sign Up</button>
           </NavLink>
         </form>
